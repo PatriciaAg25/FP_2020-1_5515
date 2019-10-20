775652f4726e6206f64c3e57b5cb4e97dd9e1167
PatriciaAg25 <ale.agui.25@hotmail.com>
2019-10-20 16:10:20 -0500

    Tarea terminada

---
+++ TAREAS/4/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]){
+	int lista[argc], suma=0;
+	float promedio;
+
+	for (int i=1; i<argc; i++){
+	lista[i]=atoi(argv[i]);
+	}
+
+	for (int i=1; i<argc; i++){
+		suma=suma+lista[i];
+	}
+
+	promedio=(float)suma/(argc-1);
+	printf("%.2f\n", promedio);
+
+	return 0;
+}
