979651e6dac5278e3f33cdd461c942a6e606a98d
PatriciaAg25 <ale.agui.25@hotmail.com>
2019-11-04 18:07:48 -0600

    Tarea final

---
+++ TAREAS/7/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]){
+	float potencia;
+	float x;
+	float y;
+	x=atof(argv[1]);
+	y=atof(argv[2]);
+
+	potencia = pow(x, y);
+	printf("%.2lf\n", potencia);
+	return 0;
+
+}
---
+++ TAREAS/8/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]){
+	float fah;
+	float cel;
+	float x;
+	float y;
+	x=atof(argv[1]);
+	y=atof(argv[2]);
+
+	if(x==0){
+	// Conversion a grados fahrenheit
+		fah = (1.8*y)+32;
+		printf("%.2f\n", fah);
+	}
+	if(x==1){
+		cel=(y-32)/1.8;
+		printf("%.2f\n", cel);
+	}
+}
